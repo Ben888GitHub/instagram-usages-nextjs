e8734c9e5aa1b83870739b15ea7da6ef83c3e222
Benedict <benedictryan80@gmail.com>
2023-07-19 18:24:01 +0700

    set image output to standalone

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ['flowbite.s3.amazonaws.com', 'scontent-lax3-1.cdninstagram.com']
-	}
+		domains: ['scontent-lax3-1.cdninstagram.com']
+	},
+	output: 'standalone'
 };
 
 module.exports = nextConfig;
