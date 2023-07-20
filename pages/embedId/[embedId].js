175580a3b5ddaf9b17bf232583f1df3ca1af3946
Benedict <benedictryan80@gmail.com>
2023-07-20 14:51:17 +0700

    replace ssg with next router on embed

--- pages/embedId/[embedId].js
+++ pages/embedId/[embedId].js
@@ -1,48 +1,19 @@
+import { useRouter } from 'next/router';
 import { InstagramEmbed } from 'react-social-media-embed';
-import axios from 'axios';
-function getLastPart(url) {
-	const parts = url.split('/');
-	// console.log(parts.at(-2));
-	return parts.at(-2);
-}
 
-const IgEmbed = ({ permalink }) => {
-	// console.log(permalink);
-	const splitVal = getLastPart(permalink);
-	// console.log(splitVal);
+const IgEmbed = () => {
+	const router = useRouter();
+
 	return (
 		<main className="flex flex-col items-center justify-between p-16">
 			<div className="flex justify-center m-10">
-				{permalink && (
-					<InstagramEmbed
-						url={`https://www.instagram.com/p/${splitVal}/`}
-						width="100%"
-					/>
-				)}
+				<InstagramEmbed
+					url={`https://www.instagram.com/p/${router.query.embedId}/`}
+					width="100%"
+				/>
 			</div>
 		</main>
 	);
 };
 
 export default IgEmbed;
-
-export const getStaticProps = async ({ params }) => {
-	console.log(params);
-
-	let singlePostProfile = `https://graph.instagram.com/${params.embedId}?fields=id,media_type,media_url,username,timestamp,caption,permalink&access_token=${process.env.NEXT_APP_IG_ACCESS_TOKEN}`;
-
-	const { data } = await axios.get(singlePostProfile);
-
-	console.log(data);
-
-	return {
-		props: { permalink: data.permalink }
-	};
-};
-
-export const getStaticPaths = () => {
-	return {
-		paths: [],
-		fallback: 'blocking'
-	};
-};
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,10 @@ import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import { getUserProfileAndMedia } from '@/instagram-data';
 import Link from 'next/link';
-import SocMedEmbed from '@/components/SocMedEmbed';
 
 const inter = Inter({ subsets: ['latin'] });
 
-const regex = '//p/([a-zA-Z0-9-_]+)//';
+const linkStyle = 'text-lg underline text-blue-600';
 
 export default function Home({ profileResult, mediaResult }) {
 	return (
@@ -18,36 +17,37 @@ export default function Home({ profileResult, mediaResult }) {
 				<p className="text-3xl">Hello, {profileResult.username}</p>
 			</div>
 			<div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-				{mediaResult?.data.map(({ caption, id, media_url, permalink }, idx) => (
-					<div key={idx}>
-						<Image
-							width={500}
-							height={500}
-							className=" max-w-full rounded-lg"
-							alt={caption}
-							priority
-							// loading="lazy"
-							// placeholder="empty"
-							src={media_url}
-							quality={50}
-						/>
-						<div>
-							<Link href={id} className="text-lg underline text-blue-600">
-								Open {caption} in new Page
-							</Link>
-							<br />
-							<Link
-								href={`embedId/${id}`}
-								className="text-lg underline text-blue-600"
-							>
-								Open {caption} in new Embed
-							</Link>
+				{mediaResult?.data.map(({ caption, id, media_url, permalink }, idx) => {
+					const parts = permalink.split('/').at(-2);
+					console.log(parts);
+
+					return (
+						<div key={idx}>
+							<Image
+								width={500}
+								height={500}
+								className=" max-w-full rounded-lg"
+								alt={caption}
+								priority
+								// loading="lazy"
+								// placeholder="empty"
+								src={media_url}
+								quality={50}
+							/>
+							<div>
+								<Link href={id} className={linkStyle}>
+									Open {caption} in new Page
+								</Link>
+								<br />
+								<Link href={`embedId/${parts}`} className={linkStyle}>
+									Open {caption} in new Embed
+								</Link>
+							</div>
+							<p className="text-xl">{id}</p>
 						</div>
-						<p className="text-xl">{id}</p>
-					</div>
-				))}
+					);
+				})}
 			</div>
-			{/* <SocMedEmbed /> */}
 		</main>
 	);
 }
